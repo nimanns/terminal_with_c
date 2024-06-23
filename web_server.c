f825deace865f12f0d5b7c2cb6006cc1cb3f287c
Nima Niazi <qwerty.nma@gmail.com>
2024-06-22 22:46:57 -0400

    basic web server

---
+++ web_server.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void handle_client(int client_socket) {
+    char buffer[BUFFER_SIZE];
+    int bytes_read;
+
+    bytes_read = read(client_socket, buffer, BUFFER_SIZE - 1);
+    if (bytes_read < 0) {
+        perror("read");
+        close(client_socket);
+        return;
+    }
+
+    buffer[bytes_read] = '\0';
+
+    printf("Request:\n%s\n", buffer);
+
+    const char *response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "<!DOCTYPE html>"
+        "<html>"
+        "<head><title>test server</title></head>"
+        "<body><h1>test</h1></body>"
+        "</html>";
+
+    write(client_socket, response, strlen(response));
+
+    close(client_socket);
+}
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len;
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, 10) < 0) {
+        perror("listen");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    while (1) {
+        client_addr_len = sizeof(client_addr);
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_socket < 0) {
+            perror("accept");
+            close(server_socket);
+            exit(EXIT_FAILURE);
+        }
+
+        handle_client(client_socket);
+    }
+
+    close(server_socket);
+
+    return 0;
+}
