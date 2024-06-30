6ef0d249c58eea8d88635a7c192b841764fba3ee
Nima <qwerty.nma@gmail.com>
2024-06-30 03:17:54 -0400

    wave player

---
+++ creative_coding/wav_player.c
@@ -0,0 +1,50 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+#define SDL_MAIN_HANDLED
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_AudioSpec wavSpec;
+    Uint32 wavLength;
+    Uint8 *wavBuffer;
+
+    if (SDL_LoadWAV("./generated_wave_file.wav", &wavSpec, &wavBuffer, &wavLength) == NULL) {
+        printf("Failed to load WAV file! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+    if (deviceId == 0) {
+        printf("Failed to open audio device! SDL_Error: %s\n", SDL_GetError());
+        SDL_FreeWAV(wavBuffer);
+        SDL_Quit();
+        return 1;
+    }
+
+    int success = SDL_QueueAudio(deviceId, wavBuffer, wavLength);
+    if (success < 0) {
+        printf("Failed to queue audio! SDL_Error: %s\n", SDL_GetError());
+        SDL_CloseAudioDevice(deviceId);
+        SDL_FreeWAV(wavBuffer);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_PauseAudioDevice(deviceId, 0);
+
+    while (SDL_GetQueuedAudioSize(deviceId) > 0) {
+        SDL_Delay(100);
+    }
+
+    SDL_CloseAudioDevice(deviceId);
+    SDL_FreeWAV(wavBuffer);
+    SDL_Quit();
+
+    return 0;
+}
