f97a1d20818fe04e6ddebd563e9233b9b998355c
Nima Niazi <qwerty.nma@gmail.com>
2024-07-04 23:43:48 -0400

    save list in gui todo

--- creative_coding/gui_todo.c
+++ creative_coding/gui_todo.c
@@ -1,16 +1,21 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #define ID_LISTBOX 1
 #define ID_EDIT 2
 #define ID_ADD_BUTTON 3
 #define ID_REMOVE_BUTTON 4
+#define MAX_ITEM_LENGTH 256
+#define FILENAME "todo_list.txt"
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 void AddControlsToWindow(HWND);
 void AddItem(HWND);
 void RemoveItem(HWND);
+void SaveList(HWND);
+void LoadList(HWND);
 
 HWND hListBox, hEdit, hAddButton, hRemoveButton;
 
@@ -26,8 +31,8 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
     if (!RegisterClassW(&wc))
         return -1;
 
-    CreateWindowW(L"ToDoListClass", L"To-Do List", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-                  100, 100, 400, 500, NULL, NULL, NULL, NULL);
+    HWND hWnd = CreateWindowW(L"ToDoListClass", L"To-Do List", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                              100, 100, 400, 500, NULL, NULL, NULL, NULL);
 
     MSG msg = {0};
     while (GetMessage(&msg, NULL, 0, 0))
@@ -45,6 +50,7 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
     {
     case WM_CREATE:
         AddControlsToWindow(hWnd);
+        LoadList(hWnd);
         break;
     case WM_COMMAND:
         switch (wp)
@@ -56,6 +62,10 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
             RemoveItem(hWnd);
             break;
         }
+        break;
+    case WM_CLOSE:
+        SaveList(hWnd);
+        DestroyWindow(hWnd);
         break;
     case WM_DESTROY:
         PostQuitMessage(0);
@@ -83,7 +93,7 @@ void AddControlsToWindow(HWND hWnd)
 
 void AddItem(HWND hWnd)
 {
-    char item[256];
+    char item[MAX_ITEM_LENGTH];
     GetWindowTextA(hEdit, item, sizeof(item));
 
     if (strlen(item) > 0)
@@ -100,4 +110,44 @@ void RemoveItem(HWND hWnd)
     {
         SendMessage(hListBox, LB_DELETESTRING, selectedIndex, 0);
     }
+}
+
+void SaveList(HWND hWnd)
+{
+    FILE *file = fopen(FILENAME, "w");
+    if (file == NULL)
+    {
+        MessageBoxW(hWnd, L"Error saving list", L"Error", MB_OK | MB_ICONERROR);
+        return;
+    }
+
+    int count = SendMessage(hListBox, LB_GETCOUNT, 0, 0);
+    for (int i = 0; i < count; i++)
+    {
+        char item[MAX_ITEM_LENGTH];
+        SendMessageA(hListBox, LB_GETTEXT, i, (LPARAM)item);
+        fprintf(file, "%s\n", item);
+    }
+
+    fclose(file);
+}
+
+void LoadList(HWND hWnd)
+{
+    FILE *file = fopen(FILENAME, "r");
+    if (file == NULL)
+    {
+        // File doesn't exist, which is fine for a new user
+        return;
+    }
+
+    char item[MAX_ITEM_LENGTH];
+    while (fgets(item, sizeof(item), file))
+    {
+        // Remove newline character
+        item[strcspn(item, "\n")] = 0;
+        SendMessageA(hListBox, LB_ADDSTRING, 0, (LPARAM)item);
+    }
+
+    fclose(file);
 }
