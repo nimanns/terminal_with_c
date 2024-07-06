c6852ff22ff5b834fe3692150b1b10e28533cb00
Nima Niazi <qwerty.nma@gmail.com>
2024-07-05 22:36:47 -0400

    gui_audio_player - added a browse button instead of a text box for file selection.

--- creative_coding/gui_audio_player.c
+++ creative_coding/gui_audio_player.c
@@ -1,11 +1,12 @@
 #include <windows.h>
 #include <mmsystem.h>
 #include <stdio.h>
+#include <commdlg.h>
 
 #pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "comdlg32.lib")
 
-HWND hPlayButton, hStopButton;
-HWND hFilePathEdit;
+HWND hPlayButton, hStopButton, hBrowseButton;
 char szFileName[MAX_PATH] = "";
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
@@ -52,23 +53,43 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
         case WM_CREATE:
-            hFilePathEdit = CreateWindow("EDIT", "", WS_CHILD | WS_VISIBLE | WS_BORDER,
-                                         10, 10, 260, 20, hwnd, (HMENU)1, NULL, NULL);
+            hBrowseButton = CreateWindow("BUTTON", "Browse", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+                                         10, 10, 100, 30, hwnd, (HMENU)1, NULL, NULL);
             hPlayButton = CreateWindow("BUTTON", "Play", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                                       10, 40, 100, 30, hwnd, (HMENU)2, NULL, NULL);
+                                       10, 50, 100, 30, hwnd, (HMENU)2, NULL, NULL);
             hStopButton = CreateWindow("BUTTON", "Stop", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                                       120, 40, 100, 30, hwnd, (HMENU)3, NULL, NULL);
+                                       120, 50, 100, 30, hwnd, (HMENU)3, NULL, NULL);
             break;
 
         case WM_COMMAND:
             switch (LOWORD(wParam)) {
-                case 2:
-                    GetWindowText(hFilePathEdit, szFileName, MAX_PATH);
-                    if (strlen(szFileName) > 0) {
-                        PlaySound(szFileName, NULL, SND_FILENAME | SND_ASYNC);
+                case 1: // Browse button
+                    {
+                        OPENFILENAME ofn;
+                        ZeroMemory(&ofn, sizeof(ofn));
+                        ofn.lStructSize = sizeof(ofn);
+                        ofn.hwndOwner = hwnd;
+                        ofn.lpstrFilter = "WAV Files (*.wav)\0*.wav\0All Files (*.*)\0*.*\0";
+                        ofn.lpstrFile = szFileName;
+                        ofn.nMaxFile = MAX_PATH;
+                        ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+                        ofn.lpstrDefExt = "wav";
+
+                        if (GetOpenFileName(&ofn)) {
+                            MessageBox(hwnd, szFileName, "File Selected", MB_OK);
+                        }
                     }
                     break;
-                case 3:
+                case 2: // Play button
+                    if (strlen(szFileName) > 0) {
+                        if (!PlaySound(szFileName, NULL, SND_FILENAME | SND_ASYNC)) {
+                            MessageBox(hwnd, "Error playing the file", "Error", MB_OK | MB_ICONERROR);
+                        }
+                    } else {
+                        MessageBox(hwnd, "Please select a WAV file first", "Error", MB_OK | MB_ICONINFORMATION);
+                    }
+                    break;
+                case 3: // Stop button
                     PlaySound(NULL, NULL, 0);
                     break;
             }
