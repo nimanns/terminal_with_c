e49b76735386ddc1abf7d00cc3f347a1da4cbe4c
Nima Niazi <qwerty.nma@gmail.com>
2024-07-29 23:22:15 -0400

    bouncing_ball: simple bouncing ball sketch using windows.h

---
+++ creative_coding/bouncing_ball.c
@@ -0,0 +1,110 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define BALL_SIZE 30
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+typedef struct {
+    int x, y;
+    int dx, dy;
+    COLORREF color;
+} Ball;
+
+Ball ball;
+
+void init_ball() {
+    ball.x = rand() % (WINDOW_WIDTH - BALL_SIZE);
+    ball.y = rand() % (WINDOW_HEIGHT - BALL_SIZE);
+    ball.dx = (rand() % 5) + 1;
+    ball.dy = (rand() % 5) + 1;
+    ball.color = RGB(rand() % 256, rand() % 256, rand() % 256);
+}
+
+void update_ball() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    if (ball.x <= 0 || ball.x >= WINDOW_WIDTH - BALL_SIZE) {
+        ball.dx = -ball.dx;
+        ball.color = RGB(rand() % 256, rand() % 256, rand() % 256);
+    }
+
+    if (ball.y <= 0 || ball.y >= WINDOW_HEIGHT - BALL_SIZE) {
+        ball.dy = -ball.dy;
+        ball.color = RGB(rand() % 256, rand() % 256, rand() % 256);
+    }
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    srand(time(NULL));
+
+    const char CLASS_NAME[] = "Bouncing Ball Window Class";
+
+    WNDCLASS wc = {0};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        "Bouncing Ball",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+		SetTimer(hwnd, 1, 16, NULL);
+    ShowWindow(hwnd, nCmdShow);
+
+    init_ball();
+
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        HBRUSH brush = CreateSolidBrush(ball.color);
+        SelectObject(hdc, brush);
+
+        Ellipse(hdc, ball.x, ball.y, ball.x + BALL_SIZE, ball.y + BALL_SIZE);
+
+        DeleteObject(brush);
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+
+    case WM_TIMER:
+        update_ball();
+        InvalidateRect(hwnd, NULL, TRUE);
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
