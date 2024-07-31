3e49623da7beb70f638df9ecfe7fb46afb15f61a
Nima Niazi <qwerty.nma@gmail.com>
2024-07-30 22:36:01 -0400

    implement circular pattern for pixel drawing

--- creative_coding/pixels.c
+++ creative_coding/pixels.c
@@ -1,15 +1,18 @@
 #include <windows.h>
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     int width = 800;
     int height = 600;
     HDC hdc = GetDC(NULL);
     int x, y;
+    float time = 0;
 
     for (int i = 0; i < 1000; i++) {
-        x = rand() % width;
-        y = rand() % height;
+        time += 0.1;
+        x = width / 2 + sin(time) * width / 4;
+        y = height / 2 + cos(time) * height / 4;
         SetPixel(hdc, x, y, RGB(rand() % 256, rand() % 256, rand() % 256));
         Sleep(10);
     }
