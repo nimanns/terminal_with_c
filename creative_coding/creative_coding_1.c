1503307353cf87b5db8bb42134b3a29288e2a605
Nima Niazi <qwerty.nma@gmail.com>
2024-08-08 23:58:36 -0400

    creative_coding_1.c

--- creative_coding/creative_coding_1.c
+++ creative_coding/creative_coding_1.c
@@ -5,11 +5,37 @@ LRESULT CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
 void draw_fractal(HDC hdc, int x, int y, int size, int depth);
 
 int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprev_instance, LPSTR lpcmd_line, int ncmd_show) {
-    // ... (same as previous)
+    WNDCLASS wc = {0};
+    wc.lpfnWndProc = window_procedure;
+    wc.hInstance = hinstance;
+    wc.lpszClassName = "fractal_window";
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindow("fractal_window", "Fractal Tree", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 800, 600, NULL, NULL, hinstance, NULL);
+
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
 }
 
 LRESULT CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
-    // ... (same as previous)
+    switch (msg) {
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            draw_fractal(hdc, 400, 550, 100, 5);
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+    return DefWindowProc(hwnd, msg, wp, lp);
 }
 
 void draw_fractal(HDC hdc, int x, int y, int size, int depth) {
