eea43c2f6db5290a28d5f073eeb53d13cf65dcb5
Nima Niazi <qwerty.nma@gmail.com>
2024-08-08 23:57:13 -0400

    enhancement: add_recursive_branching_to_fractal

--- creative_coding/creative_coding_1.c
+++ creative_coding/creative_coding_1.c
@@ -1,7 +1,7 @@
 #include <windows.h>
 
 LRESULT CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
-void draw_fractal(HDC hdc, int x, int y, int size);
+void draw_fractal(HDC hdc, int x, int y, int size, int depth);
 
 int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprev_instance, LPSTR lpcmd_line, int ncmd_show) {
     // ... (same as previous)
@@ -12,7 +12,7 @@ LRESULT CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
         case WM_PAINT: {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
-            draw_fractal(hdc, 400, 550, 100);
+            draw_fractal(hdc, 400, 550, 100, 5);
             EndPaint(hwnd, &ps);
             return 0;
         }
@@ -23,7 +23,16 @@ LRESULT CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
     return DefWindowProc(hwnd, msg, wp, lp);
 }
 
-void draw_fractal(HDC hdc, int x, int y, int size) {
+void draw_fractal(HDC hdc, int x, int y, int size, int depth) {
+    if (depth == 0) return;
+
     MoveToEx(hdc, x, y, NULL);
     LineTo(hdc, x, y - size);
+
+    int new_x = x;
+    int new_y = y - size;
+
+    draw_fractal(hdc, new_x, new_y, size * 0.7, depth - 1);
+    draw_fractal(hdc, new_x - size / 2, new_y, size * 0.7, depth - 1);
+    draw_fractal(hdc, new_x + size / 2, new_y, size * 0.7, depth - 1);
 }
